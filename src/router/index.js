addcabb8f0137b7e0a3ff619443c435585538a8e
IT-Seal <2529043070@qq.com>
2023-06-11 23:00:12 +0800

    wbb

--- src/router/index.js
+++ src/router/index.js
@@ -115,7 +115,7 @@ const routes = [
 ]
 //创建路由管理 router
 const router = createRouter({// 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 router.beforeEach((to, from, next) => {
